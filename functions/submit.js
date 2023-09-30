58225d0fe452049e32cbcc15c942cb5df8c26c93
erichayth <42790061+erichayth@users.noreply.github.com>
2023-09-30 13:43:09 -0400

    working example

--- functions/submit.js
+++ functions/submit.js
@@ -1,29 +1,47 @@
+const SECRET_KEY = process.env.CF_SECRET_KEY;
+
 /**
  * POST /api/submit
  */
 export async function onRequestPost(context) {
     try {
-      let input = await context.request.formData();
-  
-      // Convert FormData to JSON
-      // NOTE: Allows multiple values per key
-      let output = {};
-      for (let [key, value] of input) {
-        let tmp = output[key];
-        if (tmp === undefined) {
-          output[key] = value;
-        } else {
-          output[key] = [].concat(tmp, value);
+        const request = context.request;
+        const body = await request.formData();
+        const token = body.get('cf-turnstile-response');
+        const ip = request.headers.get('CF-Connecting-IP');
+
+        let formData = new FormData();
+        formData.append('secret', SECRET_KEY);
+        formData.append('response', token);
+        formData.append('remoteip', ip);
+
+        const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+        const result = await fetch(url, { body: formData, method: 'POST' });
+        const outcome = await result.json();
+
+        if (!outcome.success) {
+            throw new Error('Invalid request');
         }
-      }
-  
-      let pretty = JSON.stringify(output, null, 2);
-      return new Response(pretty, {
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-        },
-      });
+
+        // Convert FormData to JSON
+        // NOTE: Allows multiple values per key
+        let output = {};
+        for (let [key, value] of body) {
+            let tmp = output[key];
+            if (tmp === undefined) {
+                output[key] = value;
+            } else {
+                output[key] = [].concat(tmp, value);
+            }
+        }
+
+        let pretty = JSON.stringify(output, null, 2);
+        return new Response(pretty, {
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+        });
     } catch (err) {
-      return new Response('Error parsing JSON content', { status: 400 });
+        return new Response('Error parsing JSON content', { status: 400 });
     }
-  }
+}
