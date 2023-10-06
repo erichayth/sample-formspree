f63eaefb647b715f32fb6498794750b4e2a93415
erichayth <42790061+erichayth@users.noreply.github.com>
2023-10-05 21:20:37 -0400

    working example 2

--- functions/submit.js
+++ functions/submit.js
@@ -53,7 +53,7 @@ export async function onRequestPost(context) {
           
             return new Response('Submission successful', {
               headers: {
-                'Content-Type': 'text/plain;charset=utf-8',
+                'Content-Type': 'application/json;charset=utf-8',
               },
             });
           } catch (err) {
